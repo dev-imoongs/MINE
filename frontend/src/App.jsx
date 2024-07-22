0ea1e781ab6ddc7cfc155c9b42282b35f6b7cec8
dev-imoongs <dev.imoong@gmail.com>
2024-07-22 13:25:40 +0900

    init commit

---
+++ frontend/src/App.jsx
@@ -0,0 +1,8 @@
+import './styles/App.css';
+import Home from './renderer/pages/Home';
+
+function App() {
+    return <Home />;
+}
+
+export default App;
---
+++ frontend/src/renderer/containers/HomeContainer.jsx
@@ -0,0 +1,8 @@
+import React from 'react';
+import HomeComponents from '../components/HomeComponents';
+
+const HomeContainer = () => {
+    return <HomeComponents />;
+};
+
+export default HomeContainer;
