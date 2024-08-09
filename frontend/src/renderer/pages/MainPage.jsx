6c31172149e1036fb1b71b6167a222828b53740c
coder-juyeon <122768623+coder-juyeon@users.noreply.github.com>
2024-08-09 22:33:33 +0900

    app.Router.jsx Outlet으로 header/main/footer 레이아웃 구성

--- frontend/src/renderer/pages/MainPage.jsx
+++ frontend/src/renderer/pages/MainPage.jsx
@@ -2,12 +2,13 @@ import React from "react";
 import HeaderContainer from "../containers/Common/HeaderContainer"
 import FooterContainer from "../containers/Common/FooterContainer"
 import MainContainer from "../containers/MainContainer"
+import MainSlideComponent from "../components/Main/MainSlideComponent.jsx";
+import MainContentComponent from "../components/Main/MainContentComponent.jsx";
 const MainPage = () => {
   return (
     <>
-        <HeaderContainer />
-        <MainContainer />
-        <FooterContainer />
+        <MainSlideComponent />
+        <MainContentComponent/>
     </>
   );
 };
--- frontend/src/routes/AppRouter.jsx
+++ frontend/src/routes/AppRouter.jsx
@@ -1,11 +1,31 @@
 import React from "react";
 import { Route, Routes } from "react-router-dom";
 import MainPage from "../renderer/pages/MainPage";
+import Mypage from "../renderer/pages/Mypage.jsx";
+import { Outlet } from "react-router-dom";
+
+import HeaderContainer from "../renderer/containers/Common/HeaderContainer.jsx"
+import FooterContainer from "../renderer//containers/Common/FooterContainer"
 const AppRouter = () => {
     return(
         <Routes>
-            <Route path="/" element={<MainPage />} />
+            <Route element={<Layout/>}>
+                <Route path="/" element={<MainPage />} />
+                <Route path="/mypage" element={<Mypage />} />
+            </Route>
         </Routes>
+    )
+}
+
+function Layout() {
+    return (
+        <>
+            <HeaderContainer/>
+                <main className="relative flex-grow border-b-2" style={{ minHeight: "0px", height: "auto" }}>
+                    <Outlet/>
+                </main>
+            <FooterContainer/>
+        </>
     )
 }
 export default AppRouter;
