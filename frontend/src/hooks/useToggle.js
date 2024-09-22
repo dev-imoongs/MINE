278424e0b46799ed956a27b9e17f2e8b5f74fe0c
code-hyun <122762287+code-hyun@users.noreply.github.com>
2024-09-22 17:49:12 +0900

    hook : useDropdown 분리

--- frontend/src/hooks/useToggle.js
+++ frontend/src/hooks/useToggle.js
@@ -10,27 +10,3 @@ export const useToggle = (initialToggleState = false) => {
 
     return [state, toggle];
 };
-
-export const useDropdown = () => {
-    const [isOpen, setIsOpen] = useState(false);
-    const ref = useRef(null);
-    const toggle = () => setIsOpen((prev) => !prev);
-    const open = () => setIsOpen(true);
-    const close = () => setIsOpen(false);
-
-    useEffect(() => {
-        const handleClickOutside = (event) => {
-            if (ref.current && !ref.current.contains(event.target)) {
-                close();
-            }
-        };
-
-        document.addEventListener('mousedown', handleClickOutside);
-
-        return () => {
-            document.removeEventListener('mousedown', handleClickOutside);
-        };
-    }, []);
-
-    return { ref, isOpen, toggle, open, close };
-};
