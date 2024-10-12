a8f93d59d38473dda5e189d70f0165a51823b6b7
dev-imoongs <dev.imoong@gmail.com>
2024-10-13 04:52:33 +0900

    상세페이지 결제 예정

--- frontend/src/renderer/pages/AuctionDetailPage.jsx
+++ frontend/src/renderer/pages/AuctionDetailPage.jsx
@@ -1,30 +1,49 @@
-import React from "react";
-import SwipeImgComponent from "../components/Auction/SwipeImgComponent";
-import ProductInfoContainer from "../containers/Auction/ProductInfoContainer";
-import ProductDetailContainer from "../containers/Auction/ProductDetailContainer";
+import React, { useEffect, useState } from 'react';
+import SwipeImgComponent from '../components/Common/SwipeImgComponent';
+import ProductInfoContainer from '../containers/Auction/ProductInfoContainer';
+import ProductDetailContainer from '../containers/Auction/ProductDetailContainer';
+import { useQuery } from 'react-query';
+import { auctionDetail } from '../../services/auctionApiService';
+import PayModal from './modal/PayModal';
 
 const AuctionDetailPage = () => {
-  const StImg = {
-    position: "absolute",
-    height: "100%",
-    width: "100%",
-    inset: "0px",
-    color: "transparent",
-  };
+    const StImg = {
+        position: 'absolute',
+        height: '100%',
+        width: '100%',
+        inset: '0px',
+        color: 'transparent',
+    };
+    const [detailInfo, setDetailInfo] = useState(null);
+    const [payIsOpen, setPayIsOpen] = useState(false);
 
-  return (
-    <div className="flex flex-col min-hscreen default-height">
-      <main className="relative flex-grow border-b-2 default-height">
-        <div className="mx-auto md:px-8 2xl:px-16 box-content max-w-[1024px] min-[1600px]:max-w-[1280px] lg:min-h-[950px] px-5 default-height">
-          <div className="items-start block grid-cols-2 lg:grid gap-x-[72px] lg:pt-[72px] lg:pb-[52px] pb-9 pt-0">
-            <SwipeImgComponent StImg={StImg} />
-            <ProductInfoContainer StImg={StImg} />
-          </div>
-          <ProductDetailContainer />
+    const detail = useQuery({
+        queryKey: 'auctionDetailData',
+        queryFn: auctionDetail,
+    });
+
+    useEffect(() => {
+        if (detail.data) {
+            setDetailInfo(detail.data);
+        }
+    }, [detail.data]);
+
+    return (
+        <div className="flex flex-col min-hscreen default-height">
+            {detailInfo && (
+                <main className="relative flex-grow border-b-2 default-height">
+                    <div className="mx-auto md:px-8 2xl:px-16 box-content max-w-[1024px] min-[1600px]:max-w-[1280px] lg:min-h-[950px] px-5 default-height">
+                        <div className="items-start block grid-cols-2 lg:grid gap-x-[72px] lg:pt-[72px] lg:pb-[52px] pb-9 pt-0">
+                            <SwipeImgComponent StImg={StImg} />
+                            <ProductInfoContainer StImg={StImg} detailInfo={detailInfo} setPayIsOpen={setPayIsOpen} />
+                        </div>
+                        <ProductDetailContainer detailInfo={detailInfo} />
+                    </div>
+                </main>
+            )}
+            {payIsOpen && <PayModal detailInfo={detailInfo} setPayIsOpen={setPayIsOpen} />}
         </div>
-      </main>
-    </div>
-  );
+    );
 };
 
 export default AuctionDetailPage;
--- frontend/src/services/auctionApiService.js
+++ frontend/src/services/auctionApiService.js
@@ -1,12 +1,17 @@
-import axios from "axios";
+import axios from 'axios';
 
 export const myAuctionProduct = async () => {
-  const response = await axios.get("/data/myAuctionData.json");
-  console.log(response.data);
-  return response.data;
+    const response = await axios.get('/data/myAuctionData.json');
+    console.log(response.data);
+    return response.data;
 };
 
 export const auctionItems = async () => {
-  const res = await axios.get("/data/auctionItemData.json");
-  return res.data;
+    const res = await axios.get('/data/auctionItemData.json');
+    return res.data;
+};
+
+export const auctionDetail = async () => {
+    const res = await axios.get('/data/auctionDetail.json');
+    return res.data;
 };
