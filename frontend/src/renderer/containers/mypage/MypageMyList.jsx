3e7358668b17018ee7d6bedf9a2357f6a5115687
coder-juyeon <122768623+coder-juyeon@users.noreply.github.com>
2024-11-27 22:35:41 +0900

    경매 UI 변경

--- frontend/src/renderer/containers/mypage/MypageMyList.jsx
+++ frontend/src/renderer/containers/mypage/MypageMyList.jsx
@@ -2,10 +2,13 @@ import React, {useState} from "react";
 import { activeIndexAtom } from "../../../recoil/atoms/userAtom.js"
 import {useNavigate, useNavigation} from 'react-router-dom';
 
+import ItemComponent from '../../components/Auction/ItemComponent';
+
 import styled from "styled-components";
 import {useRecoilState} from "recoil";
 const MypageMyList = ({data,isProduct}) => {
-    const condition = ["판매내역", "판매완료", "구매내역", "찜한 상품"];
+    const productCondition = ["판매내역", "판매완료", "구매내역", "찜한 상품"];
+    const auctionCondition = ["경매내역", "경매완료", "구매내역", "찜한 상품"];
     const filter = ["최신순", "낮은가격순", "높은가격순"];
     const [openMenuIndex, setOpenMenuIndex] = useState(null); // 현재 열린 메뉴의 인덱스를 저장
     const [activeIndex, setActiveIndex] = useRecoilState(activeIndexAtom);
@@ -43,7 +46,7 @@ const MypageMyList = ({data,isProduct}) => {
               </MyListTitle>
               <MyListSubTitle>
                   <ul className="colors flex flex-nowrap justify-between lg:justify-start -me-3 border-b border-[#DADEE5]">
-                      {condition.map((item, index) => (
+                      {(isProduct ? productCondition : auctionCondition).map((item, index) => (
                           <li
                               key={index}
                               className={`shrink grow lg:grow-0 cursor-pointer py-4 basis-[84px] lg:basis-[160px] flex justify-center items-center font-medium transition duration-200 ease-in-out ${
@@ -194,7 +197,7 @@ const MypageMyList = ({data,isProduct}) => {
                       </div>
                   ) : (
                       <div className="my-item-con relative" key={index}>
-                      <div className="my-content-con relative">
+                          <div className="my-content-con relative">
                               <a
                                   className="my-content-link group box-border overflow-hidden flex rounded-md cursor-pointer pe-0 pb-2 lg:pb-3 flex-col items-start transition duration-200 ease-in-out transform bg-white"
                                   title={item.AUCTION_ITEM_NAME}
@@ -217,44 +220,48 @@ const MypageMyList = ({data,isProduct}) => {
                                               color: "transparent"
                                           }}
                                       />
-                                  </div>
-                                  <div
-                                      className="my-text-con w-full overflow-hidden p-0 md:p-0 lg:p-0 xl:p-0 2xl:p-0">
-                                      <h2 className="line-clamp-2 min-h-[10h] text-sm md:text-base text-heading">
-                                          {item.AUCTION_ITEM_NAME}
-                                          <span className="text-sm text-gray-400 mx-1">|</span>
-                                          <span>{item.AUCTION_ITEM_STATUS_NAME}</span>
-                                      </h2>
-                                      <div className="font-semibold space-s-2 mt-0.5 text-heading lg:text-lg lg:mt-1.5">
-                                          {item.AUCTION_ITEM_STATUS === 1
-                                              ? `${item.AUCTION_ITEM_START_PRICE}원`
-                                              : `${item.AUCTION_ITEM_HIGHEST_PRICE}원`}
-                                          {item.AUCTION_ITEM_STATUS !== 1 &&
-                                              <span className={"m-1"}>시작가: {item.AUCTION_ITEM_START_PRICE}</span>}
-                                      </div>
-                                      <div className="my-1">
-                                          <span
-                                              className="text-sm text-gray-400">{item.AUCTION_ITEM_STATUS === 1 ? '등록시간' : '종료시간'}</span>
-                                          <span className="text-sm text-gray-400 mx-1">|</span>
-                                          <span
-                                              className="text-sm text-gray-400">{item.AUCTION_ITEM_STATUS === 1 ? item.AUCTION_ITEM_CREATE_TIME : item.AUCTION_ITEM_END_TIME}</span>
-                                      </div>
-                                      {item.AUCTION_ITEM_STATUS === 2 && (
-                                          <div className="my-1">
-                                              <span className="text-sm text-gray-400">남은시간</span>
-                                              <span className="text-sm text-gray-400 mx-1">|</span>
-                                              <span
-                                                  className="text-sm text-gray-400">01D 01H
-                                      </span>
-                                              <span
-                                                  className="text-sm text-gray-400">
-                                          <span className={"m-1"}>입찰수</span>
-                                          <span>{item.AUCTION_JOIN_COUNT}회</span>
-                                      </span>
-                                          </div>
+                                      {item.AUCTION_ITEM_STATUS == 1 ? (
+                                          <div className="z-10 pending item-status">대기</div>
+                                      ): (
+                                          <div className="z-10 ongoing item-status">진행</div>
                                       )}
+                                      {/*<div className="z-10 pending item-status">대기</div>*/}
+                                      {/*<div className="z-10 ongoing item-status">진행</div>*/}
+                                      {/*<div className="z-10 ended item-status">종료</div>*/}
                                   </div>
                               </a>
+                              <div className="w-full overflow-hidden p-2 md:px-2.5 xl:px-4">
+                                  <h2 className="line-clamp-2 min-h-[2lh] text-sm md:text-base">{item.AUCTION_ITEM_NAME}</h2>
+                                  <div className="mt-2">
+                                      <span className="font-bold">{item.AUCTION_ITEM_HIGHEST_PRICE}</span>
+                                      <span className="ml-2 text-s text-gray-500 line-through">{item.AUCTION_ITEM_START_PRICE}</span>
+                                  </div>
+                                  {/*<div className="font-semibold space-s-2 mt-0.5 text-heading lg:text-lg lg:mt-1.5">*/}
+                                  {/*    {price}원*/}
+                                  {/*</div>*/}
+                                  <div className="item-info_wrap">
+                                      <div>
+                                          <span>입찰 </span>
+                                          <span>{item.AUCTION_JOIN_COUNT}회</span>
+                                      </div>
+                                      <span className="bar"></span>
+                                      <div className="item-info time">
+                                          <svg
+                                              xmlns="http://www.w3.org/2000/svg"
+                                              width="14"
+                                              height="14"
+                                              viewBox="0 0 14 14"
+                                              fill="none"
+                                          >
+                                              <path
+                                                  d="M13.5443 3.2199C13.7953 2.9243 13.757 2.48076 13.4591 2.23252L11.311 0.442462C11.0169 0.19744 10.5804 0.235108 10.3327 0.526876C10.0817 0.822472 10.12 1.26601 10.4179 1.51425L12.566 3.30431C12.8601 3.54933 13.2966 3.51167 13.5443 3.2199ZM3.57846 1.5113C3.87485 1.26442 3.91379 0.823528 3.66526 0.528522C3.41887 0.23606 2.98262 0.197294 2.68853 0.441729L0.541829 2.22593C0.243509 2.47388 0.204934 2.91764 0.455997 3.21334C0.703548 3.50491 1.13978 3.54259 1.43367 3.2978L3.57846 1.5113ZM7.35 4.79312C7.35 4.50317 7.11495 4.26812 6.825 4.26812C6.53505 4.26812 6.3 4.50317 6.3 4.79312V7.87071C6.3 8.22307 6.48544 8.54942 6.78814 8.72978L9.20096 10.1674C9.43606 10.3075 9.74008 10.2326 9.88328 9.99943C10.0298 9.76086 9.95196 9.44852 9.71067 9.30655L7.84289 8.20758C7.5375 8.0279 7.35 7.70003 7.35 7.3457V4.79312ZM7 1.48759C3.521 1.48759 0.7 4.28898 0.7 7.74379C0.7 11.1986 3.514 14 7 14C10.479 14 13.3 11.1986 13.3 7.74379C13.3 4.28898 10.479 1.48759 7 1.48759ZM7 12.6097C4.291 12.6097 2.1 10.434 2.1 7.74379C2.1 5.05363 4.291 2.87786 7 2.87786C9.709 2.87786 11.9 5.05363 11.9 7.74379C11.9 10.434 9.709 12.6097 7 12.6097Z"
+                                                  fill="#626262"
+                                              />
+                                          </svg>
+                                          <span className="pl-[8px]">7일 00시간 </span>
+                                      </div>
+                                  </div>
+                              </div>
                           </div>
                           <div
                               className="function-con flex flex-col space-y-2 items-end absolute cursor-pointer right-0 top-3 w-full h-auto">
