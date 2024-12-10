b159f8bbe41f29d8605b36a77c52f2a2ea5ef084
code-hyun <122762287+code-hyun@users.noreply.github.com>
2024-12-10 22:25:56 +0900

    backend - MainPage 데이터 통신 확인
    - DB -> Controller 데이터 확인
    - /used-item/getItem 요청 후 응답확인

--- backend/Mine/src/main/java/com/app/mine/service/UsedItemServiceImpl.java
+++ backend/Mine/src/main/java/com/app/mine/service/UsedItemServiceImpl.java
@@ -5,6 +5,7 @@ import com.app.mine.vo.UsedItemVO;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
+import java.util.List;
 
 @Service
 @RequiredArgsConstructor
@@ -17,4 +18,11 @@ public class UsedItemServiceImpl implements UsedItemService {
     public void saveUsedItem(UsedItemVO usedItemVO) {
         usedItemMapper.insertUsedItem(usedItemVO);
     }
+
+    @Override
+    public List<UsedItemVO> getAllUsedItems() {
+        return usedItemMapper.findAll();
+    }
+
+
 }
