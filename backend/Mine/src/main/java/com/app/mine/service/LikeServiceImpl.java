bedf435fc1547f8e4c33010cc0f8a13226764ea8
code-hyun <122762287+code-hyun@users.noreply.github.com>
2025-01-22 23:49:21 +0900

    중고거래 상세페이지 Like 완료

--- backend/Mine/src/main/java/com/app/mine/service/LikeServiceImpl.java
+++ backend/Mine/src/main/java/com/app/mine/service/LikeServiceImpl.java
@@ -5,7 +5,10 @@ import com.app.mine.vo.LikeVO;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
+import java.util.HashMap;
+import java.util.Map;
 import java.util.Optional;
 
 @Slf4j
@@ -15,22 +18,51 @@ public class LikeServiceImpl implements LikeService{
 
     private final LikeMapper likeMapper;
 
+    @Transactional
     @Override
-    public void toggleLike(Integer userId, Integer usedItemId, Integer auctionItemId) {
+    public Map<String,Object> toggleLike(Integer userId, Integer usedItemId, Integer auctionItemId) {
+        Map<String, Object> resultMap = new HashMap<>();
 
-            // 중고,경매 아이템에 대한 좋아요 처리
+        try {
             Optional<LikeVO> existingLike = likeMapper.findByUserIdAndItemId(userId, usedItemId, auctionItemId);
             log.info("select");
+
             if (existingLike.isPresent()) {
-                // 좋아요가 있다면 삭제
+                // 좋아요가 존재하면 삭제
                 likeMapper.deleteByUserIdAndItemId(userId, usedItemId, auctionItemId);
                 log.info("delete");
+                resultMap.put("result", "success");
+                resultMap.put("type", "delete");
             } else {
-                // 좋아요가 없다면 추가
+                // 좋아요가 없으면 추가
                 likeMapper.insertLikeForItem(userId, usedItemId, auctionItemId);
                 log.info("insert");
+                resultMap.put("result", "success");
+                resultMap.put("type", "insert");
             }
+        } catch (Exception e) {
+            log.error("Error during toggleLike: {}", e.getMessage());
+            resultMap.put("result", "fail");
+            throw e;
+        }
 
+        return resultMap;
+    }
+
+    @Override
+    public Map<String, Object> findLikeStateById(Integer userId, Integer usedItemId, Integer auctionItemId) {
+        Map<String, Object> resultMap = new HashMap<>();
+        try {
+            Optional<LikeVO> existingLike = likeMapper.findByUserIdAndItemId(userId, usedItemId, auctionItemId);
+            if (existingLike.isPresent()) {
+                resultMap.put("type", "insert");
+            } else {
+                resultMap.put("type", "delete");
+            }
+        } catch (Exception e) {
+            throw e;
+        }
+        return resultMap;
     }
 
 }
